f810ff5febcd7a2e00db1e2169ae1e233b2af2a4
mickey2099beyond <43194494+mickey2099beyond@users.noreply.github.com>
2018-10-02 22:59:49 -0500

    mejorado

--- guardar.php
+++ guardar.php
@@ -1,12 +1,12 @@
 <?php
 //conectar con el servidor
-$conectar=@mysql_connect('loacalhost','root','');
+$conectar=@mysql_connect('localhost','root','');
 //verificacion de laconexion
 if(!$conectar)
 {
 	echo"No se pudo conectar con el servidor";
 }else{
-$base=mysqll_select_db("bd_prueba");
+$base=mysql_select_db('bd_prueba');
 if(!$base){
 echo "No se encontro la base de datos";
 	}
@@ -15,17 +15,17 @@ echo "No se encontro la base de datos";
 $first_name=$_POST['first_name'];
 $last_name=$_POST['last_name'];
 $email=$_POST['email']
-$nombre=$_POST['password'];
-$nombre=$_POST['password_confirmation'];
+$password=$_POST['password'];
+$password_confirmation=$_POST['password_confirmation'];
 
-$sql="INSERT INTO datos VALUES('$first_name','$last_name','$email','$password','$password_confirmation')";
-
+$sql="INSERT INTO registro (first_name,last_name,email,password,password_confirmation) VALUES ('$first_name','$last_name','$email','$password','$password_confirmation')";
+//(first_name,last_name,email,password,password_confirmation)
 //ejecutar la sentencia
 $ejecutar=mysql_query($sql);
 //verificar la ejecucion
 if(!$ejecutar){
-echo="Hubo algun error";
+echo"Hubo algun error";
 }else{
-echo"Datos guardados correctamente <br><a href='index.php'>Volver</a>"
+echo"Datos guardados correctamente";
 }
 ?>
