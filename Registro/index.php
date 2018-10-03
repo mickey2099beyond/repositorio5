5f57d6d96e8f21c36333064282eb191040dd094d
mickey2099beyond <43194494+mickey2099beyond@users.noreply.github.com>
2018-10-02 20:06:59 -0500

    ok

---
+++ Registro/index.php
@@ -0,0 +1,66 @@
+<?php
+
+include'conexion.php';
+?>
+
+<div class="container" style="margin-top:5em;">
+  <br>
+  <br>
+  <div class="row">
+  <div class="col-md-4"></div>
+  <div class="col-md-4">
+    <div class="container">
+      <div class="row centered-form">
+      <div class="">
+        <div class="panel panel-default">
+          <div class="panel-heading text-center">
+            <h3 class="panel-title">Bienvenidos Por Favor Registrese</h3>
+          </div>
+          <div class="panel-body">
+            <form role="form">
+              <form action="guardar" method="POST">
+              <div class="row">
+                <div class="col-xs-6 col-sm-6 col-md-6">
+                  <div class="form-group">
+                    <input type="text" name="first_name" id="first_name" class="form-control input-sm" placeholder="First Name">
+                  </div>
+                </div>
+                <div class="col-xs-6 col-sm-6 col-md-6">
+                  <div class="form-group">
+                    <input type="text" name="last_name" id="last_name" class="form-control input-sm" placeholder="Last Name">
+                  </div>
+                </div>
+              </div>
+
+              <div class="form-group">
+                <input type="email" name="email" id="email" class="form-control input-sm" placeholder="Email">
+              </div>
+
+              <div class="row">
+                <div class="col-xs-6 col-sm-6 col-md-6">
+                  <div class="form-group">
+                    <input type="password" name="password" id="password" class="form-control input-sm" placeholder="Password">
+                  </div>
+                </div>
+                <div class="col-xs-6 col-sm-6 col-md-6">
+                  <div class="form-group">
+                    <input type="password" name="password_confirmation" id="password_confirmation" class="form-control input-sm" placeholder="Password Confirmation">
+                  </div>
+                </div>
+              </div>
+
+              <input type="submit" value="Register" class="btn btn-info btn-block">
+
+            </form>
+          </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  </div>
+    <div>
+  <div class="col-md-4"></div>
+      </div>
+</div>
+</div>
---
+++ bd_prueba.sql
@@ -0,0 +1,63 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 03-10-2018 a las 02:53:01
+-- Versión del servidor: 10.1.30-MariaDB
+-- Versión de PHP: 5.6.33
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `bd_prueba`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `registro`
+--
+
+CREATE TABLE `registro` (
+  `id` int(11) NOT NULL,
+  `first_name` varchar(50) NOT NULL,
+  `last_name` varchar(50) NOT NULL,
+  `email` varchar(80) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `password_confirmation` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `registro`
+--
+ALTER TABLE `registro`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `registro`
+--
+ALTER TABLE `registro`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
